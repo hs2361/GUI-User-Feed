b43c0926b287d36f2df2f50ad67b259fcbf02998
Harsh Sharma <51477130+hs2361@users.noreply.github.com>
2019-06-25 14:20:21 +0530

    Joke of the day

--- joke.py
+++ joke.py
@@ -5,3 +5,4 @@ j = f"http://www.laughfactory.com/jokes/clean-jokes/{page}"
 joke_soup = bs(urllib.request.urlopen(j))
 joke = joke_soup.select(".joke-text p")
 print(re.split('"joke_[0-9][0-9][0-9][0-9][0-9]">\r\n',str(random.choice(joke)))[1].replace("</p>",'').replace("<br/>",'\n').strip())
+    
