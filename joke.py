7dec6bffa5489c57d637f0cc9bf9438650f89972
Harsh Sharma <51477130+hs2361@users.noreply.github.com>
2019-07-03 11:24:44 +0530

    Deleted Files

--- joke.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup as bs
-import urllib,re,random
-page = random.randint(1,34)
-j = f"http://www.laughfactory.com/jokes/clean-jokes/{page}"
-joke_soup = bs(urllib.request.urlopen(j))
-joke = joke_soup.select(".joke-text p")
-print(re.split('"joke_[0-9][0-9][0-9][0-9][0-9]">\r\n',str(random.choice(joke)))[1].replace("</p>",'').replace("<br/>",'\n').strip())
-    
--- weather.py
+++
@@ -1,9 +0,0 @@
-import requests,json
-city = input("Enter the name of a city: ")
-response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city.lower().capitalize()}&APPID=213fc9be40420ed21ae1365c8dd63666&units=metric")
-data = response.json()
-
-print(f"Weather description of {city.lower().capitalize()}: {data['weather'][0]['description'].capitalize()}")
-print(f"The temperature in {city.lower().capitalize()} is " + str(data['main']['temp']) + "°C")
-print(f"The humidity in {city.lower().capitalize()} is {data['main']['humidity']}%")
-print(f"The wind is blowing at a speed of {data['wind']['speed']} m/s")
